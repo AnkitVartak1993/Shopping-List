a691944fe34fbaeb4e2dff8ca351b42ce721e582
AnkitVartak1993 <ankitvartak@gmail.com>
2018-03-25 12:36:46 -0400

    Added add-page view for admin

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+//GET index page
 router.get('/',(req,res)=>{
     res.render('index',{title:'Home'});
 });
 
+//GET add page
+router.get('/add-page',(req,res)=>{
+   // res.render('index',{title:'Home'});
+   var title;
+   var slug;
+   var content;
+
+   res.render('admin/add_page',{
+        title: title,
+        slug: slug,
+        content: content
+   });
+});
 
 module.exports =  router;
