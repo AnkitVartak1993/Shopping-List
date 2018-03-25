a2edf023ae79babc23787ad17bc6dea74369b81b
AnkitVartak1993 <ankitvartak@gmail.com>
2018-03-25 11:48:20 -0400

    Added other libs and created message-alert template

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const {dbString} = require('./config/dbconfig');
+const bodyParser = require('body-parser');
+var session = require('express-session')
+var expressValidator = require('express-validator');
 
 var app = express();
 //DB connect
@@ -20,10 +23,50 @@ app.set('view engine', 'ejs');
 //set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//body-parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+//express-session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+
+//express-validator middleware setup
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+                , root = namespace.shift()
+                , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+//express-messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 //set routes
 var pages = require('./routes/pages.js');
 var adminPages = require('./routes/admin_pages.js');
-app.use('/admin/pages', adminPages)
+app.use('/admin/pages', adminPages);
 app.use('/', pages);
 
 
