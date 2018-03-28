cb594d85aa54b2ed07d727d6f14774bc5eae13b7
AnkitVartak1993 <ankitvartak@gmail.com>
2018-03-27 22:12:10 -0400

    Added pages reordering feature

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -80,4 +80,25 @@ router.post('/add-page',(req,res)=>{
 });
 
 
+//Sort pages 
+router.post('/reorder-pages',(req,res)=>{
+    var ids = req.body['id[]'];
+    var count = 0;
+
+    for (var i = 0; i < ids.length; i++) {
+        var id = ids[i];
+        count++;
+
+        (function (count) {
+            Page.findById(id, function (err, page) {
+                page.sorting = count;
+                page.save(function (err) {
+                    if (err)
+                        return console.log(err);
+                });
+            });
+        })(count);
+
+    }
+})
 module.exports =  router;
