27ab75488d54d0a07bec13cab2e16058d3f0b00c
AnkitVartak1993 <ankitvartak@gmail.com>
2018-03-31 00:13:08 -0400

    Added git post edit route

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -79,6 +79,78 @@ router.post('/add-page',(req,res)=>{
     }
 });
 
+//POST edit page
+router.post('/edit-page/:slug',(req,res)=>{
+   // res.render('index',{title:'Home'});
+   req.checkBody('title', 'Title must have a value').notEmpty();
+   req.checkBody('content', 'Content must have a value').notEmpty();
+   
+   var title = req.body.title;
+   var content = req.body.content;
+    var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+   if (slug == "")
+    slug = title.replace(/\s+/g, '-').toLowerCase();
+   var id = req.body.id;
+   var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('admin/edit_page', {
+            errors: errors,
+            title: title,
+            slug: slug,
+            content: content,
+            id:id
+        });
+    } else {
+
+        Page.findOne({slug:slug, _id:{'$ne':id}},(err, page)=>{
+            if(page){
+                req.flash('danger', 'Page slug exists, choose another.');
+                res.render('admin/edit_page', {
+                    title: title,
+                    slug: slug,
+                    content: content,
+                    id: id
+                });
+            } else {
+                    Page.findById(id, (err, page)=>{
+                        if(err) 
+                        return console.log(err);
+
+                        page.title = title;
+                        page.slug = slug;
+                        page.content = content;
+
+                        page.save((err)=>{
+                        if(err) return console.log(err);
+                    
+                        req.flash('success', 'Page Added!');
+                        res.redirect('/admin/pages/edit-page/'+page._id);
+                    });
+                    });
+                   
+            }
+        });
+        console.log('success');
+    }
+});
+
+router.get('/edit-page/:id',(req,res)=>{
+    Page.findById(req.params.id, (err,page)=>{
+        if(err){
+            return console.log(err);
+        }
+        res.render('admin/edit_page',{
+            title: page.title,
+            slug: page.slug,
+            content: page.content,
+            id: page._id
+        })
+    })
+    
+})
+
+
 
 //Sort pages 
 router.post('/reorder-pages',(req,res)=>{
