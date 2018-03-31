fdecb7ba0d2ca27463ea312f5ba7f96c801e2f16
AnkitVartak1993 <ankitvartak@gmail.com>
2018-03-31 13:41:21 -0400

    Added Product Model and installed express-fileupload, fs-extra packages

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+//page schema
+var ProductSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    desc:{
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    sorting:{
+        type: Number,
+    },
+    image:{
+        type: String
+    },
+
+    slug:{
+        type: String
+    }
+});
+
+var Product = module.exports = mongoose.model('Product', PageSchema);
