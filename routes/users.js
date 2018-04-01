d85dba50863c8e238ac2f676e65957042645b52a
AnkitVartak1993 <ankitvartak@gmail.com>
2018-04-01 15:17:39 -0400

    Added POST register

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,7 @@ var bcrypt = require('bcryptjs');
 // Get Users model
 var User = require('../models/user');
 
-/*
- * GET register
- */
+// GET register
 router.get('/register', function (req, res) {
 
     res.render('register', {
@@ -17,6 +15,68 @@ router.get('/register', function (req, res) {
 
 });
 
+//POST register
+router.post('/register', function (req, res) {
+
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    req.checkBody('name', 'Name is required!').notEmpty();
+    req.checkBody('email', 'Email is required!').isEmail();
+    req.checkBody('username', 'Username is required!').notEmpty();
+    req.checkBody('password', 'Password is required!').notEmpty();
+    req.checkBody('password2', 'Passwords do not match!').equals(password);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register', {
+            errors: errors,
+            user: null,
+            title: 'Register'
+        });
+    } else {
+        User.findOne({email: email}, function (err, user) {
+            if (err)
+                console.log(err);
+
+            if (user) {
+                req.flash('danger', 'Email exists, choose another!');
+                res.redirect('/users/register');
+            } else {
+                var user = new User({
+                    name: name,
+                    email: email,
+                    username: username,
+                    password: password,
+                    admin: 0
+                });
+
+                bcrypt.genSalt(10, function (err, salt) {
+                    bcrypt.hash(user.password, salt, function (err, hash) {
+                        if (err)
+                            console.log(err);
+
+                        user.password = hash;
+
+                        user.save(function (err) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                req.flash('success', 'You are now registered!');
+                                res.redirect('/users/login')
+                            }
+                        });
+                    });
+                });
+            }
+        });
+    }
+
+});
 
 // Exports
 module.exports = router;
