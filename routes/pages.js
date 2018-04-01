419c50f00f28389cb2e8ffde5ac7ebf5c4ff65e6
AnkitVartak1993 <ankitvartak@gmail.com>
2018-04-01 01:53:25 -0400

    Added Display Menu pages Content on Index page

--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.render('index',{ title:'Pages'});
+// Get Page model
+var Page = require('../models/page');
+
+//Get Page Home
+router.get('/', function (req, res) {
+    
+    Page.findOne({slug: 'home'}, function (err, page) {
+        if (err)
+            console.log(err);
+
+        res.render('index', {
+            title: page.title,
+            content: page.content
+        });
+    });   
 });
 
+//Get Selected Menu
+router.get('/:slug', function (req, res) {
+
+    var slug = req.params.slug;
+
+    Page.findOne({slug: slug}, function (err, page) {
+        if (err)
+            console.log(err);
+        
+        if (!page) {
+            res.redirect('/');
+        } else {
+            res.render('index', {
+                title: page.title,
+                content: page.content
+            });
+        }
+    }); 
+});
 
 module.exports =  router;
