11ce54125cf2836f6d6e730e1e0e355d49611178
AnkitVartak1993 <ankitvartak@gmail.com>
2018-04-01 03:02:06 -0400

    Added Products by category

--- routes/products.js
+++ routes/products.js
@@ -24,6 +24,24 @@ router.get('/', function (req, res) {
 
 });
 
+//GET products by category
+router.get('/:category', function (req, res) {
+
+    var categorySlug = req.params.category;
+
+    Category.findOne({slug: categorySlug}, function (err, c) {
+        Product.find({category: categorySlug}, function (err, products) {
+            if (err)
+                console.log(err);
+
+            res.render('cat_products', {
+                title: c.title,
+                products: products
+            });
+        });
+    });
+
+});
 
 // Exports
 module.exports = router;
